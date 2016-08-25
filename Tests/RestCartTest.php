8b8e521b0bf8cb95f08ceaadea3f736040ca2abc
Roberto Dugim <roberto.dugim@gmail.com>
2016-08-25 17:46:20 -0300

    I forget to remove a print_r
    rs

--- Tests/RestCartTest.php
+++ Tests/RestCartTest.php
@@ -122,7 +122,6 @@ class RestCartTest extends PHPUnit_Framework_TestCase{
 		$response = self::$client->get('cart/get_cart');
 		$this->assertEquals(200, $response->getStatusCode());
 		$responseBody = json_decode($response->getBody(),true);
-		print_r($responseBody);
 		$this->assertInternalType('array',$responseBody);
 		$this->assertEquals('success', $responseBody['result']);
 	}
