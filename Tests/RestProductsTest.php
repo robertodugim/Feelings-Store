4b6a8f3b267416d9919cfd656e4e2c4144f76158
Roberto Dugim <roberto.dugim@gmail.com>
2016-08-25 16:52:47 -0300

    Rest API Test with Guzzle
    update in .htaccess to make api url more friendly

---
+++ Tests/RestProductsTest.php
@@ -0,0 +1,25 @@
+<?php
+use GuzzleHttp\Client;
+
+class RestProductsTest extends PHPUnit_Framework_TestCase{
+	
+	protected static $url;
+	protected static $client;
+	
+	public static function setUpBeforeClass(){
+		self::$url = "http://localhost/Feelings-Store/api";
+		self::$client = new \GuzzleHttp\Client(['cookies' => true,'base_uri' => 'http://localhost/Feelings-Store/api/']);
+		
+	}
+	
+	/** @test */
+	public function get_products_list(){
+		$response = self::$client->get('products/get_list');
+		$this->assertEquals(200, $response->getStatusCode());
+		$responseBody = json_decode($response->getBody(),true);
+		$this->assertInternalType('array',$responseBody);
+		$this->assertEquals('success', $responseBody['result']);
+	}
+	
+}
+?>
